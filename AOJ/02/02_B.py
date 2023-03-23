3d66dd665ebaae3906eed87c97b35c18db643cbc
“kotatakahasi-git” <kotataka2001@gmail.com>
2023-03-24 02:04:24 +0900

    Initial commit

---
+++ AOJ/02/02_B.py
@@ -0,0 +1,5 @@
+a,b,c=(int(x) for x in input().split())
+if a<b and b<c and a<c:
+    print('Yes')
+else:
+    print('No')    
---
+++ AOJ/02/02_C.py
@@ -0,0 +1,4 @@
+a,b,c=(int(x) for x in input().split())
+l = [a,b,c]
+l.sort()
+print(l[0],l[1],l[2])
