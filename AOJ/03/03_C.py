27d7ea61bad1c1b7651bebd53d35ab56062d6d7a
kotatakahashi-git <126054459+kotatakahashi-git@users.noreply.github.com>
2023-03-31 19:20:58 +0900

    Update AOJ/03/03_C.py
    Co-authored-by: takeshun <75155218+takeshun256@users.noreply.github.com>

--- AOJ/03/03_C.py
+++ AOJ/03/03_C.py
@@ -2,7 +2,7 @@ while True:
     x,y=(int(a) for a in input().split())
     if  x == 0 and y == 0: 
       break
-    elif x <= y :
+    if x <= y :
           print(x,y)
     else : 
         print(y,x)
