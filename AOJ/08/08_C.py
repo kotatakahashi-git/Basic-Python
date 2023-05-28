ba5032e7bd3a04b15bc01678155541190d4e9e9a
kotatakahashi-git <126054459+kotatakahashi-git@users.noreply.github.com>
2023-05-28 21:30:57 +0900

    Update 08_C.py

--- AOJ/08/08_C.py
+++ AOJ/08/08_C.py
@@ -3,5 +3,5 @@ a=input()
 for line in sys.stdin:
     a+=line
 a=a.lower()
-for str in list("abcdefghijklmnopqrstuvwxyz"):
-    print(*[str,":",a.count(str)])
+for char in list("abcdefghijklmnopqrstuvwxyz"):
+    print(*[char,":",a.count(char)])
